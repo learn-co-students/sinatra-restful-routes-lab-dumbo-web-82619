992016fc8789d9c1f6d212888dab9ee11a179471
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-09-18 22:31:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
   delete '/recipes/:id' do
     recipe = Recipe.find(params[:id])
     recipe.destroy
-    redirect to '/'
+    redirect to '/recipes'
   end
 
 end
